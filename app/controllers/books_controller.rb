5495c3df327295e8ef9eb0c10ec041c3b807e287
DWC-user07- <124852092+DWC-user07->
2023-03-14 01:31:59 +0000

    [Fix]booksコントローラ,ルーテイング,views編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,25 @@ class BooksController < ApplicationController
   def index
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(books_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+  def books_params
+    params.require(:books).permit(:books_title, :books_body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   devise_for :users
   root 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
