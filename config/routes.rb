92e00170392e19a095ef8b5dd5e083507659d722
DWC-user07- <124852092+DWC-user07->
2023-03-14 04:19:53 +0000

    [Remove]books,usersコントローラとアクションを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
