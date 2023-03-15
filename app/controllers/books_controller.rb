509b619e7a5db00148d5978179c4e57c75c40feb
DWC-user07- <124852092+DWC-user07->
2023-03-15 03:25:33 +0000

    [Fix]削除機能作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,38 @@
 class BooksController < ApplicationController
-
+# books投稿一覧
   def index
     @books = Book.all
     @book = Book.new
   end
-
+# books作成
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book)
   end
-
+# books詳細
   def show
     @book = Book.find(params[:id])
     @book = Book.new
+    @books = Book.all
   end
-
+# books編集
   def edit
+    @books = Book.find(params[:id])
+  end
+  # books更新
+  def update
     @book = Book.find(params[:id])
+    @book.update(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  # books削除
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
