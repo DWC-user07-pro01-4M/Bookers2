7e6eb97118feea17b30abe9997188cbb86f901a9
DWC-user07- <124852092+DWC-user07->
2023-03-15 05:48:32 +0000

    [Add/Fix]Usersにユーザが一覧表示されるように記述また修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @book = Book.new
+    @user = User.find(current_user.id)
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
